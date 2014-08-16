88a2e4b32d8917378eefd6fc3989e656b91d893e
Brian Jenkins <bonkydog@bonkydog.com>
2014-08-16 10:06:01 -0700

    Thank role models.

--- init.el
+++ init.el
@@ -1,7 +1,29 @@
-;;;; Brian Jenkins's Emacs Configuration
-;;;
+;;;; Brian Jenkins (bonkdog)'s Emacs Configuration
+
 ;;; "Your father's lightsaber. This is the weapon of a Jedi
 ;;; Knight. Not as clumsy or random as a blaster. An elegant weapon,
 ;;; for a more civilized age."
 ;;;
 ;;; ―Obi-Wan Kenobi
+;;;
+;;;
+;;; Many thanks to these Emacs Jedi, from whom I have learned (and,
+;;; honestly, copied) so much.
+;;;
+;;; Aaron Bedra
+;;; http://www.aaronbedra.com/emacs.d
+;;;
+;;; Bozhidar Batsov
+;;; https://github.com/bbatsov/prelude
+;;;
+;;; Dimitri Fontaine
+;;; https://github.com/dimitri/emacs-kicker
+;;;
+;;; Magnar Sveen
+;;; https://github.com/magnars/.emacs.d
+;;;
+;;; Phil Hagelberg (technomancy)
+;;; https://github.com/technomancy/emacs-starter-kit/tree/master
+;;;
+;;; Sam Aaron
+;;; https://github.com/overtone/emacs-live
