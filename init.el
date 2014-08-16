4853073822d5863ec4aebc80ac2e10f9fd5ce411
Brian Jenkins <bonkydog@bonkydog.com>
2014-08-16 08:00:26 -0700

    first!

---
+++ init.el
@@ -0,0 +1,7 @@
+;;;; Brian Jenkins's Emacs Configuration
+;;;
+;;; "Your father's lightsaber. This is the weapon of a Jedi
+;;; Knight. Not as clumsy or random as a blaster. An elegant weapon,
+;;; for a more civilized age."
+;;;
+;;; ―Obi-Wan Kenobi
