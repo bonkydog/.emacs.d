cdd2a27a0a5acca929f893f0177bab90048d869e
Brian Jenkins <bonkydog@bonkydog.com>
2014-10-01 17:58:02 -0700

    Apply MacOS specific customizations.

--- init.el
+++ init.el
@@ -137,6 +137,10 @@
 
 (require 'dircolors)
 
+;; Mac's ls doesn't support the --dired flag.  Fall back to emacs's ls emulation.
+(when (eq system-type 'darwin)
+  (require 'ls-lisp)
+  (setq ls-lisp-use-insert-directory-program nil))
 
 ;;; Navigate ELisp source without a tags file
 
@@ -375,3 +379,35 @@
 ;;; Clojure Refactoring
 
 (require 'clj-refactor)
+
+
+;;; Customize for MacOS
+
+(when (eq system-type 'darwin)
+  (setq default-input-method "MacOSX")
+
+  ;; Make cut and paste work with the OS X clipboard
+
+  (defun live-copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+  (defun live-paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+	(process-send-string proc text)
+	(process-send-eof proc))))
+
+  (when (not window-system)
+    (setq interprogram-cut-function 'live-paste-to-osx)
+    (setq interprogram-paste-function 'live-copy-from-osx))
+
+  ;; Work around a bug on OS X where system-name is a fully qualified
+  ;; domain name
+  (setq system-name (car (split-string system-name "\\.")))
+
+  ;; Ensure the exec-path honours the shell PATH
+  (require 'exec-path-from-shell)
+  (exec-path-from-shell-initialize)
+
+  ;; Ignore .DS_Store files with ido mode
+  (add-to-list 'ido-ignore-files "\\.DS_Store"))
