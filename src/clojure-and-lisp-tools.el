5069d36974b76255cdd009f44c28a1e0025f87e5
Brian Jenkins <bonkydog@bonkydog.com>
2014-10-01 20:50:35 -0700

    Copy more clojure config from Live.
    align-cljlet, mic-paren, rainbow delimiters

--- src/clojure-and-lisp-tools.el
+++ src/clojure-and-lisp-tools.el
@@ -9,6 +9,11 @@
 (require 'paredit)
 (require 'thingatpt)
 
+(require 'mic-paren)
+(paren-activate)
+
+(require 'rainbow-delimiters)
+
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'clojure-mode-hook    'enable-paredit-mode)
 
@@ -177,16 +182,6 @@
 (define-key paredit-mode-map (kbd "C-M-k")   'live-paredit-copy-sexp-at-point)
 
 
-;;; Snippets
-
-(require 'yasnippet)
-
-
-;;; Multiple Cursors
-
-(require 'multiple-cursors)
-
-
 ;;; Clojure Mode
 
 (require 'clojure-mode)
@@ -198,7 +193,8 @@
 
 
 ;;; Clojure Refactoring
-
+(require 'yasnippet)
+(require 'multiple-cursors)
 (require 'clj-refactor)
 
 ;;; Paredit
@@ -402,6 +398,121 @@
 
 (require 'clj-refactor)
 
+
+;;; Emacs Live Clojure conf
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "λ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\)("
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "ƒ")
+                               nil))))))
+
+(eval-after-load 'clojure-mode
+  '(font-lock-add-keywords
+    'clojure-mode `(("\\(#\\){"
+                     (0 (progn (compose-region (match-beginning 1)
+                                               (match-end 1) "∈")
+                               nil))))))
+
+(eval-after-load 'find-file-in-project
+  '(add-to-list 'ffip-patterns "*.clj"))
+
+(require 'clojure-mode)
+
+(add-hook 'clojure-mode-hook
+          (lambda ()
+            (setq buffer-save-without-query t)))
+
+;;command to align let statements
+;;To use: M-x align-cljlet
+(require 'align-cljlet)
+
+;;Treat hyphens as a word character when transposing words
+(defvar clojure-mode-with-hyphens-as-word-sep-syntax-table
+  (let ((st (make-syntax-table clojure-mode-syntax-table)))
+    (modify-syntax-entry ?- "w" st)
+    st))
+
+(defun live-transpose-words-with-hyphens (arg)
+  "Treat hyphens as a word character when transposing words"
+  (interactive "*p")
+  (with-syntax-table clojure-mode-with-hyphens-as-word-sep-syntax-table
+    (transpose-words arg)))
+
+(define-key clojure-mode-map (kbd "M-t") 'live-transpose-words-with-hyphens)
+
+(setq auto-mode-alist (append '(("\\.cljs$" . clojure-mode))
+                              auto-mode-alist))
+
+(dolist (x '(scheme emacs-lisp lisp clojure))
+  (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'enable-paredit-mode)
+  (add-hook (intern (concat (symbol-name x) "-mode-hook")) 'rainbow-delimiters-mode))
+
+;; Pull in the awesome clj-refactor lib by magnars
+(require 'clj-refactor)
+(add-hook 'clojure-mode-hook (lambda ()
+                               (clj-refactor-mode 1)
+                               (cljr-add-keybindings-with-prefix "C-c C-m")))
+
+(define-key clojure-mode-map (kbd "C-:") 'cljr-cycle-stringlike)
+(define-key clojure-mode-map (kbd "C->") 'cljr-cycle-coll)
+
+(defun live-warn-when-cider-not-connected ()
+      (interactive)
+      (message "nREPL server not connected. Run M-x cider or M-x cider-jack-in to connect."))
+
+(define-key clojure-mode-map (kbd "C-M-x")   'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-x C-e") 'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-e") 'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-l") 'live-warn-when-cider-not-connected)
+(define-key clojure-mode-map (kbd "C-c C-r") 'live-warn-when-cider-not-connected)
+
+
+;;; Emacs Live Cider conf
+
+(require 'cider)
+
+(defun live-windows-hide-eol ()
+ "Do not show ^M in files containing mixed UNIX and DOS line endings."
+ (interactive)
+ (setq buffer-display-table (make-display-table))
+ (aset buffer-display-table ?\^M []))
+
+(when (eq system-type 'windows-nt)
+  (add-hook 'nrepl-mode-hook 'live-windows-hide-eol ))
+
+(add-hook 'cider-repl-mode-hook
+          (lambda ()
+            (cider-turn-on-eldoc-mode)
+            (paredit-mode 1)))
+
+(add-hook 'cider-mode-hook
+           (lambda ()
+             (cider-turn-on-eldoc-mode)
+             (paredit-mode 1)))
+
+(setq cider-popup-stacktraces t)
+(setq cider-popup-stacktraces-in-repl t)
+(add-to-list 'same-window-buffer-names "*cider*")
+
+
+;; Specify the print length to be 100 to stop infinite sequences killing
+;; things. This might be dangerous for some people relying on
+;; *print-length* being larger. Consider a work around
+(defun live-nrepl-set-print-length ()
+  (nrepl-send-string-sync "(set! *print-length* 100)" "clojure.core"))
+
+(add-hook 'nrepl-connected-hook 'live-nrepl-set-print-length)
+
+(setq nrepl-port "4555")
 
 ;;; Emacs Live lisp conf
 
