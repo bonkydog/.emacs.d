81e3134fae750ae254076cd71f3193289737ec31
Brian Jenkins <bonkydog@bonkydog.com>
2014-10-01 19:21:14 -0700

    Extract chunks of related stuff to files.

--- init.el
+++ init.el
@@ -57,13 +57,13 @@
 
 (defun bonkydog-set-up-load-path () ; This is itempotent.  Feel free to re-run it if you add libraries.
   ;; my code
-  (add-to-list 'load-path (concat user-emacs-directory "src/"))
+  (add-to-list 'load-path (expand-file-name "src" bonkydog-root-dir))
 
   ;; other peoples' "vendored" code.  (That is, checked into this project.)
-  (add-to-list 'load-path (concat user-emacs-directory "vendor/")) ; others' code, checked in.
+  (add-to-list 'load-path (expand-file-name "vendor" bonkydog-root-dir)) ; others' code, checked in.
 
   ;; other people's code, "submoduled" into this project
-  (dolist (dir (directory-files (concat user-emacs-directory "lib/") t))
+  (dolist (dir (directory-files (expand-file-name "lib" bonkydog-root-dir) t))
     (if (and (file-directory-p dir)
              (not (string-prefix-p "." (file-name-nondirectory dir))))
         (add-to-list 'load-path dir))))
@@ -138,6 +138,12 @@
 (auto-compile-on-save-mode 1)
 
 
+;;; Navigate ELisp source without a tags file
+
+(require 'elisp-slime-nav)
+(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
+
+
 ;;; Dired configuration
 
 (require 'dircolors)
@@ -146,11 +152,6 @@
 (when (eq system-type 'darwin)
   (require 'ls-lisp)
   (setq ls-lisp-use-insert-directory-program nil))
-
-;;; Navigate ELisp source without a tags file
-
-(require 'elisp-slime-nav)
-(add-hook 'emacs-lisp-mode-hook 'elisp-slime-nav-mode)
 
 
 ;;; Fuzzy matching
@@ -184,334 +185,15 @@
 (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 
-;;; Paredit
-
-(eval-after-load 'paredit
-  ;; need a binding that works in the terminal
-  '(define-key paredit-mode-map (kbd "M-)") 'paredit-forward-slurp-sexp))
-
-(require 'paredit)
-(require 'thingatpt)
-
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'clojure-mode-hook    'enable-paredit-mode)
-
-;; Paredit extensions from Emacs Live
-
-(defun live-paredit-next-top-level-form ()
-  (interactive)
-  (while (ignore-errors (paredit-backward-up) t))
-  (live-paredit-forward))
-
-(defun live-paredit-previous-top-level-form ()
-  (interactive)
-  (if (ignore-errors (paredit-backward-up) t)
-      (while (ignore-errors (paredit-backward-up) t))
-    (paredit-backward)))
-
-(defun live-paredit-forward ()
-  "Feels more natural to move to the beginning of the next item
-   in the sexp, not the end of the current one."
-  (interactive)
-  (if (and (not (paredit-in-string-p))
-           (save-excursion
-             (ignore-errors
-               (forward-sexp)
-               (forward-sexp)
-               t)))
-      (progn
-        (forward-sexp)
-        (forward-sexp)
-        (backward-sexp))
-    (paredit-forward)))
-
-(defun live-paredit-forward-slurp-sexp-neatly ()
-  (interactive)
-  (save-excursion
-    (cond ((or (paredit-in-comment-p)
-               (paredit-in-char-p))
-           (error "Invalid context for slurping S-expressions."))
-          ((paredit-in-string-p)
-           (paredit-forward-slurp-into-string))
-          (t
-
-           (save-excursion
-             (paredit-forward-up)
-             (paredit-backward-down)
-             (paredit-forward-slurp-sexp)
-             (just-one-space)))))
-  (when (not (save-excursion
-               (ignore-errors
-                 (backward-sexp)
-                 t)))
-    (delete-horizontal-space)))
-
-(defun live-paredit-forward-kill-sexp (&optional arg)
-  (interactive "p")
-  (cond ((or (paredit-in-comment-p)
-             (paredit-in-string-p)) (kill-word (or arg 1)))
-        (t (kill-sexp (or arg 1)))))
-
-(defun live-paredit-backward-kill-sexp (&optional arg)
-  (interactive "p")
-  (cond ((or (paredit-in-comment-p)
-             (paredit-in-string-p)) (backward-kill-word (or arg 1)))
-        (t (backward-kill-sexp (or arg 1)))))
-
-(defun live-paredit-backward-kill ()
-  (interactive)
-  (let ((m (point-marker)))
-    (paredit-backward-up)
-    (forward-char)
-    (delete-region (point) m)))
-
-(defun live-paredit-delete-horizontal-space ()
-  (interactive)
-  (just-one-space -1)
-  (paredit-backward-delete))
-
-(defun live-paredit-tidy-trailing-parens ()
-  (interactive)
-  (save-excursion
-    (while (ignore-errors (paredit-forward-up) t))
-    (backward-char)
-    (live-paredit-delete-horizontal-space)
-    (while
-        (or
-         (eq (char-before) ?\))
-         (eq (char-before) ?\})
-         (eq (char-before) ?\]))
-      (backward-char)
-      (live-paredit-delete-horizontal-space))))
-
-(defun live-paredit-reindent-defun (&optional argument)
-  "Reindent the definition that the point is on. If the point is
-  in a string or a comment, fill the paragraph instead, and with
-  a prefix argument, justify as well. Doesn't mess about with
-  Clojure fn arglists when filling-paragraph in docstrings.
-
-  Also tidies up trailing parens when in a lisp form"
-  (interactive "P")
-  (cond ((paredit-in-comment-p) (fill-paragraph argument))
-        ((paredit-in-string-p) (progn
-                                 (save-excursion
-                                   (paredit-forward-up)
-                                   (insert "\n"))
-                                 (fill-paragraph argument)
-                                 (save-excursion
-                                   (paredit-forward-up)
-                                   (delete-char 1))))
-        (t (when (not (live-paredit-top-level-p))
-             (progn (save-excursion
-                      (end-of-defun)
-                      (beginning-of-defun)
-                      (indent-sexp))
-                    (live-paredit-tidy-trailing-parens))))))
-
-
-(defun live-paredit-forward-down ()
-  "Doesn't freeze Emacs if attempted to be called at end of
-   buffer. Otherwise similar to paredit-forward-down."
-  (interactive)
-  (if (save-excursion
-          (forward-comment (buffer-size))
-          (not (live-end-of-buffer-p)))
-      (paredit-forward-down)
-    (error "unexpected end of buffer")))
-
-(defun live-paredit-top-level-p ()
-  "Returns true if point is not within a given form i.e. it's in
-  toplevel 'whitespace'"
-  (not
-   (save-excursion
-     (ignore-errors
-       (paredit-forward-up)
-       t))))
-
-(defun live-paredit-copy-sexp-at-point ()
-  (interactive)
-    (kill-new (thing-at-point 'sexp)))
-
-
-(define-key paredit-mode-map (kbd "C-c l k") 'paredit-splice-sexp-killing-forward)
-(define-key paredit-mode-map (kbd "C-c l w") 'paredit-splice-sexp-killing-backward)
-(define-key paredit-mode-map (kbd "C-c l l") 'align-cljlet)
-(define-key paredit-mode-map (kbd "C-c l t") 'fill-paragraph)
-(define-key paredit-mode-map (kbd "C-c l j") 'live-paredit-forward-slurp-sexp-neatly)
-(define-key paredit-mode-map (kbd "C-M-e")   'paredit-backward-barf-sexp)
-(define-key paredit-mode-map (kbd "C-M-s")   'paredit-backward-slurp-sexp)
-(define-key paredit-mode-map (kbd "C-M-j")   'live-paredit-forward-slurp-sexp-neatly)
-(define-key paredit-mode-map (kbd "C-M-y")   'paredit-forward-barf-sexp)
-(define-key paredit-mode-map (kbd "C-M-z")   'align-cljlet)
-(define-key paredit-mode-map (kbd "M-S")     'paredit-split-sexp)
-(define-key paredit-mode-map (kbd "M-s")     'paredit-splice-sexp)
-(define-key paredit-mode-map (kbd "M-j")     'paredit-join-sexps)
-(define-key paredit-mode-map (kbd "M-P")     'live-paredit-previous-top-level-form)
-(define-key paredit-mode-map (kbd "M-N")     'live-paredit-next-top-level-form)
-(define-key paredit-mode-map (kbd "C-M-f")   'live-paredit-forward)
-(define-key paredit-mode-map (kbd "M-q")     'live-paredit-reindent-defun)
-(define-key paredit-mode-map (kbd "M-d")     'live-paredit-forward-kill-sexp)
-(define-key paredit-mode-map (kbd "M-k")     'live-paredit-backward-kill)
-(define-key paredit-mode-map (kbd "M-\\")    'live-paredit-delete-horizontal-space)
-(define-key paredit-mode-map (kbd "C-M-i")   'paredit-forward-down)
-(define-key paredit-mode-map (kbd "C-M-n")   'paredit-forward-up)
-(define-key paredit-mode-map (kbd "C-M-p")   'paredit-backward-down)
-(define-key paredit-mode-map (kbd "C-M-u")   'paredit-backward-up)
-(define-key paredit-mode-map (kbd "M-T")     'transpose-sexps)
-(define-key paredit-mode-map (kbd "C-M-k")   'live-paredit-copy-sexp-at-point)
-
-
-;;; Snippets
-
-(require 'yasnippet)
-
-
-;;; Multiple Cursors
-
-(require 'multiple-cursors)
-
-
-;;; Clojure Mode
-
-(require 'clojure-mode)
-
-
-;;; Cider (Clojure REPL)
-
-(require 'cider)
-
-
-;;; Clojure Refactoring
-
-(require 'clj-refactor)
-
+(load "clojure-and-lisp-tools")
 
 ;;; Customize for MacOS
 
 (when (eq system-type 'darwin)
-  (setq default-input-method "MacOSX")
-
-  ;; Make cut and paste work with the OS X clipboard
-
-  (defun live-copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
-
-  (defun live-paste-to-osx (text &optional push)
-    (let ((process-connection-type nil))
-      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-        (process-send-string proc text)
-        (process-send-eof proc))))
-
-  (when (not window-system)
-    (setq interprogram-cut-function 'live-paste-to-osx)
-    (setq interprogram-paste-function 'live-copy-from-osx))
-
-  ;; Work around a bug on OS X where system-name is a fully qualified
-  ;; domain name
-  (setq system-name (car (split-string system-name "\\.")))
-
-  ;; Ensure the exec-path honours the shell PATH
-  (require 'exec-path-from-shell)
-  (exec-path-from-shell-initialize)
-
-  ;; Ignore .DS_Store files with ido mode
-  (add-to-list 'ido-ignore-files "\\.DS_Store"))
+  (load "macos-customizations")
+  (setq default-input-method "MacOSX"))
 
 
 ;; Configure Emacs built-ins
 
-;;use file path to ensure buffer name uniqueness
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-(setq uniquify-separator "/")
-(setq uniquify-after-kill-buffer-p t)
-(setq uniquify-ignore-buffers-re "^\\*")
-
-;;store history of recently opened files
-(require 'recentf)
-(setq recentf-save-file (concat bonkydog-tmp-dir "recentf")
-      recentf-max-saved-items 200)
-(recentf-mode t)
-
-;;When you visit a file, point goes to the last place where it was
-;;when you previously visited. Save file is set to bonkydog-tmp-dir/places
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file (concat bonkydog-tmp-dir "places"))
-
-;;enable cua-mode for rectangular selections
-(require 'cua-base)
-(require 'cua-gmrk)
-(require 'cua-rect)
-(cua-mode 1)
-(setq cua-enable-cua-keys nil)
-
-;;enable winner mode for C-c-(<left>|<right>) to navigate the history
-;;of buffer changes i.e. undo a split screen
-(when (fboundp 'winner-mode)
-      (winner-mode 1))
-
-(setq initial-major-mode 'lisp-interaction-mode
-      redisplay-dont-pause t
-      column-number-mode t
-      echo-keystrokes 0.02
-      inhibit-startup-message t
-      transient-mark-mode t
-      shift-select-mode nil
-      require-final-newline t
-      truncate-partial-width-windows nil
-      delete-by-moving-to-trash nil
-      confirm-nonexistent-file-or-buffer nil
-      query-replace-highlight t
-      next-error-highlight t
-      next-error-highlight-no-select t)
-
-;;set all coding systems to utf-8
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8)
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-;;disable CJK coding/encoding (Chinese/Japanese/Korean characters)
-(setq utf-translate-cjk-mode nil)
-
-(set-default 'indent-tabs-mode nil)
-(auto-compression-mode t)
-(show-paren-mode 1)
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;;default to unified diffs
-(setq diff-switches "-u"
-      ediff-window-setup-function 'ediff-setup-windows-plain)
-
-;; make emacs use the clipboard
-(setq x-select-enable-clipboard t)
-
-;;remove all trailing whitespace and trailing blank lines before
-;;saving the file
-(defvar live-ignore-whitespace-modes '(markdown-mode))
-(defun live-cleanup-whitespace ()
-  (if (not (member major-mode live-ignore-whitespace-modes))
-      (let ((whitespace-style '(trailing empty)) )
-        (whitespace-cleanup))))
-
-(add-hook 'before-save-hook 'live-cleanup-whitespace)
-
-;; savehist keeps track of some history
-(setq savehist-additional-variables
-      ;; search entries
-      '(search ring regexp-search-ring)
-      ;; save every minute
-      savehist-autosave-interval 60
-      ;; keep the home clean
-      savehist-file (concat bonkydog-tmp-dir "savehist"))
-(savehist-mode t)
-
-
-;; bookmarks
-(require 'bookmark)
-(setq bookmark-default-file (expand-file-name "bookmarks" bonkydog-tmp-dir)
-      bookmark-save-flag 1)
+(load "configure-built-ins")
